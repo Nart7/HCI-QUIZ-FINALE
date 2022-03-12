aba774ffe0878522f8da7ef4aa7ea453ce59569e
Nart7 <nart7@hotmail.co.uk>
2022-03-12 19:44:11 +0500

    Questions and Answer list in json file

---
+++ src/constants/questions.js
@@ -0,0 +1,147 @@
+export const questionsList = [
+    {
+        questionText: 'What are you sitting on?',
+        answerOptions: [
+            { answerText: 'Comfy chair', isCorrect: false },
+            { answerText: 'Standing', isCorrect: true },
+            { answerText: 'Uncomfy chair', isCorrect: false },
+            { answerText: 'Bed', isCorrect: false },
+        ],
+        description: `A better chair allows the user to sit down for extended periods
+						of time without being agitated. It also reduces the stress on your back,
+						preventing injuries whilst also correcting posture. To help this you could:
+						- purchase a back cushion
+						- adjust the height of the chair (forearms being parallel to the ground)
+						- purchase a new chair with great comfort reviews.`
+    },
+    {
+        questionText: 'Do your eyes strain behind your computer?',
+        answerOptions: [
+            { answerText: 'Yes', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'Rarely chair', isCorrect: false },
+            { answerText: 'After long periods of time', isCorrect: false },
+        ],
+        description: `Some users may experience eye strain when dealt with
+						monitors/screens. This is very common especially after long periods of
+						time. To help this you could:
+						- Purchase a pair of anti-UV glare glasses. These glasses have a subtle
+						yellow tint which helps with eye strain significantly
+						- purchase an anti-glare screen if they wish
+						- Turn down brightness of monitor`
+    },
+    {
+        questionText: 'Do your feet have space under my desk?',
+        answerOptions: [
+            { answerText: 'Yes', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'If I cleared everything out', isCorrect: false },
+            { answerText: 'Not possible', isCorrect: false },
+        ],
+        description: `If the user has a crowded foot environment they can feel
+						claustrophobic and uncomfortable. This especially occurs after long periods
+						of time. To help this you could:
+						- Clear out everything unnecessary that hinders them.
+						- Adjust the foot traffic so that you have as much space as possible.
+						- Purchase a foot hammock`
+    },
+    {
+        questionText: 'Are your computer peripherals comfortable and easy to use?',
+        answerOptions: [
+            { answerText: 'Yes', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'Some of them', isCorrect: false },
+            { answerText: 'Do not have peripherals', isCorrect: false },
+        ],
+        description: `This involves the mouse, mousepad and keyboard mainly.
+						Some of these could be uncomfortable to the wrists or thumbs. This is
+						especially true to those with physical problems such as tendinitis. To help
+						improve this, you could:
+						Purchase an ergonomic mouse that takes strain off arms.
+						- Purchase a memory-foam mouse/keyboard pad.`
+    },
+    {
+        questionText: 'Do you have desk space?',
+        answerOptions: [
+            { answerText: 'Yes, enough', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'It is possible', isCorrect: false },
+            { answerText: 'Desk is physically too small', isCorrect: false },
+        ],
+        description: `A crowded desk space can make the user uncomfortable and
+						agitated. There are studies that a tidy work environment boosts morale and
+						productivity. To help improve this, you could
+						- Clear out everything unnecessary on the table.
+						- Purchase a new table suited to the room you are in.`
+    },
+    {
+        questionText: 'Where is your monitor positioned?',
+        answerOptions: [
+            { answerText: 'High compared to eye level', isCorrect: false },
+            { answerText: 'Low compared to eye level', isCorrect: true },
+            { answerText: 'Directly ahead', isCorrect: false },
+            { answerText: 'To the left/right', isCorrect: false },
+        ],
+        description: `If your screen is either too high/low or too close/far it can
+						cause strain in the neck. To help improve this, you could:
+						- Move the monitor so you are eye level with the screen
+						- place something under the monitor to boost height.`
+    },
+    {
+        questionText: 'Are you feeling cold/hot in currently?',
+        answerOptions: [
+            { answerText: 'Hot', isCorrect: false },
+            { answerText: 'Cold', isCorrect: true },
+            { answerText: 'Perfect', isCorrect: false },
+            { answerText: 'It varies', isCorrect: false },
+        ],
+        description: `If the user is cold or hot it can make it a lot tougher to use a
+						computer. Hands could be sweaty or shivering. Either one affects HCI
+						heavily. To help improve this you could:
+						- Purchase fan
+						- Purchase air conditioner
+						- Increase clothing layering
+						- purchase an electric heater`
+    },
+    {
+        questionText: 'Are you hydrated?',
+        answerOptions: [
+            { answerText: 'Yes', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'Sometimes', isCorrect: false },
+            { answerText: 'If I had a bottle next to me, I would be.', isCorrect: false },
+        ],
+        description: `Hydration level affects users' comfort and can help the users
+						brain function, increasing productivity. To help improve this, you could:
+						- Purchase a water bottle
+						- Have a reminder on your desk to stay hydrated`
+    },
+    {
+        questionText: 'Are you feeling mentally well?',
+        answerOptions: [
+            { answerText: 'Yes', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'I am agitated', isCorrect: false },
+            { answerText: 'I am bored', isCorrect: false },
+        ],
+        description: `Some users may feel agitated for no reason at all, it is best to
+						comfort the body if this is the case. To help improve this you could:
+						-Stretch
+						- Listen to soothing music`
+    },
+    {
+        questionText: 'Is your environment noisy?',
+        answerOptions: [
+            { answerText: 'Yes', isCorrect: false },
+            { answerText: 'No', isCorrect: true },
+            { answerText: 'Sometimes', isCorrect: false },
+            { answerText: 'Always', isCorrect: false },
+        ],
+        description: `SNaturally, working in a loud environment can be challenging
+						due to all the distractions. This decreases a users satisfaction and comfort.
+						To help improve this, you could:
+						- Purchase noise canceling headset
+						- If possible, ask surrounding people to quiet down.`
+    },
+
+]
